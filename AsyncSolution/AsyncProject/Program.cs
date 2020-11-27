05a337db0c196614098a7bc96ff249e9afa94455
Baens, Ludovic <ludovic.baens@solidaris.be>
2020-11-27 13:18:04 +0100

    Version Initiale Synchrone

---
+++ AsyncSolution/AsyncProject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace AsyncProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Début du programme");
+            AppelTache1();
+            AppelTache2();
+
+            Console.WriteLine("Fin du programme");
+            Console.ReadKey();
+        }
+
+
+        public static void AppelTache1()
+        {
+            AppelMethode1();
+            Console.WriteLine("  FIN DE LA TACHE 1");
+        }
+
+        public static void AppelTache2()
+        {
+            AppelMethode2();
+            Console.WriteLine("  FIN DE LA TACHE 2");
+        }
+
+        public static void AppelMethode1()
+        {
+            Thread.Sleep(6000);
+        }
+
+        public static void AppelMethode2()
+        {
+            Thread.Sleep(4000);
+        }
+    }
+}
